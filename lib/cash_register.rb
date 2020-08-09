907cb20bf97beba3d2753f85ec241c4728ac6e8e
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-08-09 23:29:24 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,7 +7,7 @@ class CashRegister
   end
 
   def add_item(title,price,quantity = 1)
-quantity.times.do 
+  quantity.times.do 
   @items << title
       @total = @total + price*quantity
       @last_transaction_amount = price*quantity
